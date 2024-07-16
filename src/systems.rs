a84569bd0c2324c303c9dd1994f014d2413e8d7b
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-07-15 20:18:24 -0400

    init

---
+++ src/systems.rs
@@ -0,0 +1,19 @@
+use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
+
+pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+        ..default()
+    });
+}
+
+pub fn spawn_snake(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    let window = window_query.get_single().unwrap();
+}
