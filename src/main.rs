aaa6abc1179bb428d7c2209e0d6fe5948fb112fc
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-07-15 20:48:54 -0400

    add sprites for the body of the snake

--- src/main.rs
+++ src/main.rs
@@ -8,5 +8,7 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, spawn_camera)
+        .add_systems(Startup, spawn_snake)
+        .add_systems(Update, input_handler_snake)
         .run();
 }
--- src/systems.rs
+++ src/systems.rs
@@ -1,5 +1,17 @@
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
+use rand::random;
+
+// variables
+
+pub const SNAKE_SPEED: f32 = 500.0;
+
+// components
+
+#[derive(Component)]
+pub struct Snake {}
+
+// systems
 
 pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
     let window = window_query.get_single().unwrap();
@@ -16,4 +28,44 @@ pub fn spawn_snake(
     asset_server: Res<AssetServer>,
 ) {
     let window = window_query.get_single().unwrap();
+
+    let random_x = random::<f32>() * window.width();
+    let random_y = random::<f32>() * window.height();
+
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform::from_xyz(random_x, random_y, 0.0),
+            texture: asset_server.load("sprites/snake_body.png"),
+            ..default()
+        },
+        Snake {},
+    ));
+}
+
+pub fn input_handler_snake(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut player_query: Query<&mut Transform, With<Snake>>,
+    time: Res<Time>,
+) {
+    if let Ok(mut transform) = player_query.get_single_mut() {
+        let mut direction = Vec3::ZERO;
+
+        if keyboard_input.pressed(KeyCode::ArrowLeft) || keyboard_input.pressed(KeyCode::KeyA) {
+            direction += Vec3::new(-1.0, 0.0, 0.0);
+        } else if keyboard_input.pressed(KeyCode::ArrowLeft)
+            || keyboard_input.pressed(KeyCode::KeyS)
+        {
+            direction += Vec3::new(0.0, -1.0, 0.0);
+        } else if keyboard_input.pressed(KeyCode::ArrowLeft)
+            || keyboard_input.pressed(KeyCode::KeyD)
+        {
+            direction += Vec3::new(1.0, 0.0, 0.0);
+        } else if keyboard_input.pressed(KeyCode::ArrowLeft)
+            || keyboard_input.pressed(KeyCode::KeyW)
+        {
+            direction += Vec3::new(0.0, 1.0, 0.0);
+        }
+
+        transform.translation += direction * SNAKE_SPEED * time.delta_seconds();
+    }
 }
